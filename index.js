9aaedadc9e02ebe32cd3633b2b2b7dab802f4e0c
Eunice Mwenda <eunice.mwenda@student.moringaschool.com>
2023-08-01 23:09:15 +0300

    Array lab intro

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name){
+  cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift("Bob");
+
+}
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+
+//const copyOfCats= cats.slice();
+//cats.push("Broom");
+function appendCat(name){
+  name= cats.slice();
+  cats.push("Broom");
+}
+
+function  appendCat(name){
+
+  return [...cats,name]
+}
+
+function prependCat(name){
+
+  return [name,...cats]
+}
+
+function removeLastCat(){
+  return cats.slice(0,cats.length-1)
+}
+
+function removeFirstCat(){
+  return cats.slice(1)
+}
+// cats.length;
+
+// cats.push("Ralph");
+// console.log(cats);
+
+
